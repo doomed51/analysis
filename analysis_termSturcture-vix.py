40a829a842855dd8701a8ddf493dc0306249cc2d
doomed51 <doomed51@gmail.com>
2023-04-10 18:48:00 -0400

    renaming to follow naming convention

--- analysis_termSturcture-vix.py
+++ analysis_termSturcture-vix.py
@@ -36,13 +36,29 @@ vix_ts_pctContango.reset_index(inplace=True)
 # set seaborn style
 sns.set()
 
-# plot fourtosevenMoContango, and averageContango
-#
+# using seaborn, create figure with 2 subplots
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(10, 10))
+
+# using seaborn, plot fourtosevenMoContango, and averageContango
+ax1=sns.lineplot(x='Date', y='fourToSevenMoContango', data=vix_ts_pctContango, ax=ax1)
+#add black line at 0
+ax1.axhline(y=0, color='black', linestyle='-')
 #vix_ts_pctContango[['fourToSevenMoContango', 'averageContango']].plot()
+
+# using seaborn, histogram of fourtosevenMoContango
+ax4=sns.histplot(vix_ts_pctContango['fourToSevenMoContango'], bins=20, ax=ax4)
+#ax4.axvline(x=0, color='black', linestyle='-')
+
+# using seaborn, plot close
+ax3=sns.lineplot(x='Date', y='close', data=vix, ax=ax3)
+
+# using seaborn, plot averageContango
+ax2=sns.lineplot(x='Date', y='averageContango', data=vix_ts_pctContango, ax=ax2)
+ax2.axhline(y=0, color='black', linestyle='-')
 
 ## setup pairplot
 #
-sns.pairplot(vix_ts_pctContango[['currentToLastContango', 'fourToSevenMoContango', 'averageContango', 'close_pctChange', 'firstToLastTermStruct']], hue='firstToLastTermStruct', height=2.5)
+#sns.pairplot(vix_ts_pctContango[['currentToLastContango', 'fourToSevenMoContango', 'averageContango', 'close_pctChange', 'firstToLastTermStruct']], hue='firstToLastTermStruct', height=2.5)
 
 
 # Scatter of fourtosevenmonthcontango vs close_pctChange 
