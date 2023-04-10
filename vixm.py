efe5a559e1a0f36ad5acc98cacb4a106962778d0
doomed51 <doomed51@gmail.com>
2023-04-10 18:48:45 -0400

    removed debug code

--- vixm.py
+++ vixm.py
@@ -17,6 +17,9 @@ vix_ts_pctContango = vixts.getVixTermStructurePctContango(fourToSeven=True)
 # join vixm and vix close columns on date
 vixm_vix = vixm.join(vix['close'], on='Date', rsuffix='_vix')
 
+# add fourtosevenmocontango column
+vixm_vix = vixm_vix.join(vix_ts_pctContango['fourToSevenMoContango'], on='Date')
+
 # add column with vixm close - vix close
 vixm_vix['closeSpread'] = vixm_vix['close'] - vixm_vix['close_vix']
 
@@ -29,31 +32,38 @@ vixm_vix['closePctChange_vix'] = vixm_vix['close_vix'].pct_change()
 # reset index
 vixm_vix.reset_index(inplace=True)
 
+print(vixm_vix)
 
 # select only the rows where vix close > vixm close
 datesWithNegativeSpread = vixm_vix[vixm_vix['close_vix'] > vixm_vix['close']]
 
-# using seaborn, plot dualaxis lineplot of closeSpread, closePctChange, closePctChange_vix, with x axis as date, and closeSpread on a secondary axis
+# Plot results
+
+# set seaborn style
 sns.set()
-"""ax1 = sns.lineplot(x='Date', y='closeSpread', data=vixm_vix)
-ax1.legend(['vixm - vix'])
-ax2 = ax1.twinx() ## setup secondary axis
-sns.lineplot(x='Date', y='closePctChange', data=vixm_vix, ax=ax2, color='green')
-sns.lineplot(x='Date', y='closePctChange_vix', data=vixm_vix, ax=ax2, color='red')
-ax2.legend(['vixm pct change', 'vix pct change'])"""
 
-# plot historgram of closeSpread, highlight the closespread from the last 5 days in red
+# pariplot with closeSpread, closePctChange, closePctChange_vix, fourToSevenMoContango
+# add a column that is 'contango' if fourToSevenMoContango is positive, 'backwardation' if negative
+vixm_vix['contango'] = vixm_vix['fourToSevenMoContango'].apply(lambda x: 'contango' if x > 0 else 'backwardation')
+sns.pairplot(vixm_vix[['fourToSevenMoContango', 'closeSpread', 'closePctChange', 'closePctChange_vix', 'contango']], height=2.5, hue='contango')
 
-sns.distplot(vixm_vix['closeSpread'], bins=50)
+## MULTIAXIS LINEPLOT
+#
+#ax1 = sns.lineplot(x='Date', y='closeSpread', data=vixm_vix)
+#ax1.legend(['vixm - vix'])
+#ax2 = ax1.twinx() ## setup secondary axis
+#sns.lineplot(x='Date', y='fourToSevenMoContango', data=vixm_vix, ax=ax2, color='green')
 
-# on the same plot, highlight the closespread from the last 5 days in red
-#sns.distplot(datesWithNegativeSpread['closeSpread'], bins=50, color='red')
+#sns.lineplot(x='Date', y='closePctChange', data=vixm_vix, ax=ax2, color='green')
+#sns.lineplot(x='Date', y='closePctChange_vix', data=vixm_vix, ax=ax2, color='red')
+#
+####
 
-# plot heatmap of closespread vs vixm pct change
-#sns.heatmap(vixm_vix[['closeSpread', 'closePctChange']].corr(), annot=True)
+# SCATTER: closeSpread vs fourtosevenmocontango
+#sns.regplot(x='fourToSevenMoContango', y='closeSpread', data=vixm_vix, scatter=True)
 
-#sns.pairplot(datesWithNegativeSpread[['closeSpread', 'closePctChange', 'closePctChange_vix']], height=2.5)
+# LINE: closeSpread over time
+#sns.lineplot(x='Date', y='closeSpread', data=vixm_vix)
 
 plt.show()
 
-print(vixm_vix)
