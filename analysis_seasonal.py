47575c577779f1636b24f2ed4cd8851c685923e8
doomed51 <doomed51@gmail.com>
2023-04-21 07:21:16 -0400

    fixed overlapping titles/charts

--- analysis_seasonal.py
+++ analysis_seasonal.py
@@ -27,43 +27,43 @@ def plotSeasonalReturns_intraday(seasonalReturns, intervals, symbols, titles, ta
     numRows = 3
 
     # setup figure and axes
-    fig, axes = plt.subplots(nrows=numRows, ncols=numCols, figsize=(numCols*6, numRows*4))
+    fig, axes = plt.subplots(nrows=numRows, ncols=numCols, figsize=(numCols*6, numRows*4.2))
 
     # set title for figure
     fig.suptitle(title)
 
-    # bar plot for ALL history: seasonalReturns[0]
+    # bar plot for ALL history: i.e. seasonalReturns[0]
     sns.barplot(x=seasonalReturns[0].index, y='pctChange_std', data=seasonalReturns[0], ax=axes[0,0], color='grey', alpha=0.5)
     sns.barplot(x=seasonalReturns[0].index, y='pctChange_mean', data=seasonalReturns[0], ax=axes[0,0].twinx(), color='red')
     axes[0,0].set_title(titles[0]) # set title for subplot
     axes[0,0].set_xlabel('') # hide x axis title
     
     
-    # bar plot for last 60 days: seasonalReturns[1]
+    # bar plot for last 60 days: i.e. seasonalReturns[1]
     sns.barplot(x=seasonalReturns[1].index, y='pctChange_std', data=seasonalReturns[1], ax=axes[0,1], color='grey', alpha=0.5)
     sns.barplot(x=seasonalReturns[1].index, y='pctChange_mean', data=seasonalReturns[1], ax=axes[0,1].twinx(), color='red')
     axes[0,1].set_title(titles[1]) # set title for subplot
     axes[0,1].set_xlabel('') # hide x axis title
 
-    #bar plot for last 90 days: seasonalReturns[2]
+    #bar plot for last 90 days: i.e. seasonalReturns[2]
     sns.barplot(x=seasonalReturns[2].index, y='pctChange_std', data=seasonalReturns[2], ax=axes[0,2], color='grey', alpha=0.5)
     sns.barplot(x=seasonalReturns[2].index, y='pctChange_mean', data=seasonalReturns[2], ax=axes[0,2].twinx(), color='red')
     axes[0,2].set_title(titles[2]) # set title for subplot
     axes[0,2].set_xlabel('') # hide x axis title
 
-    # bar plot of last 30 days: seasonalReturns[3]
+    # bar plot of last 30 days: i.e. seasonalReturns[3]
     sns.barplot(x=seasonalReturns[3].index, y='pctChange_std', data=seasonalReturns[3], ax=axes[1,0], color='grey', alpha=0.5)
     sns.barplot(x=seasonalReturns[3].index, y='pctChange_mean', data=seasonalReturns[3], ax=axes[1,0].twinx(), color='red')
     axes[1,0].set_title(titles[3]) # set title for subplot
     axes[1,0].set_xlabel('') # hide x axis title
 
-    # bar plof of prev 30 days: seasonalReturns[4]
+    # bar plof of prev 30 days: i.e. seasonalReturns[4]
     sns.barplot(x=seasonalReturns[4].index, y='pctChange_std', data=seasonalReturns[4], ax=axes[1,1], color='grey', alpha=0.5)
     sns.barplot(x=seasonalReturns[4].index, y='pctChange_mean', data=seasonalReturns[4], ax=axes[1,1].twinx(), color='red')
     axes[1,1].set_title(titles[4]) # set title for subplot
     axes[1,1].set_xlabel('') # hide x axis title
 
-    # bar plot of prev 60 days: seasonalReturns[5]
+    # bar plot of prev 60 days: i.e. seasonalReturns[5]
     sns.barplot(x=seasonalReturns[5].index, y='pctChange_std', data=seasonalReturns[5], ax=axes[1,2], color='grey', alpha=0.5)
     sns.barplot(x=seasonalReturns[5].index, y='pctChange_mean', data=seasonalReturns[5], ax=axes[1,2].twinx(), color='red')
     axes[1,2].set_title(titles[5]) # set title for subplot
@@ -78,7 +78,7 @@ def plotSeasonalReturns_intraday(seasonalReturns, intervals, symbols, titles, ta
     sns.heatmap(seasonalReturns[8].pivot_table(index='Time', columns='Date', values='pctChange'), ax=axes[2,2])
     axes[2,2].set_title(titles[8])
 
-    # tilt x axis labels 45 degrees
+    # tilt x axis labels 90 degrees
     axes[0,0].set_xticklabels(axes[0,0].get_xticklabels(), rotation=90, fontsize=8)
     axes[0,1].set_xticklabels(axes[0,0].get_xticklabels(), rotation=90, fontsize=8)
     axes[0,2].set_xticklabels(axes[0,0].get_xticklabels(), rotation=90, fontsize=8)
@@ -124,11 +124,12 @@ def seasonalAnalysis_overview(symbol, restrictTradingHours=False, target='close'
     seasonalAggregate_1day = getSeasonalAggregate(pxHistory_1day, '1day', symbol)
     seasonalAggregate_5mins = getSeasonalAggregate(pxHistory_5mins, '5mins', symbol)
     seasonalAggregate_weekByDay = getSeasonalAggregate(pxHistory_5mins, 'weekByDay', symbol)
+    seasonalAggregate_yearByMonth = getSeasonalAggregate(pxHistory_1day, 'yearByMonth', symbol)
 
     ## construct figure and plots 
 
     # set up figure and axes with a 1x3 grid
-    fig, axes = plt.subplots(1, 3, figsize=(17, 5))
+    fig, axes = plt.subplots(2, 3, figsize=(17, 5))
 
     # add title to figure
     fig.suptitle('Overview of Seasonal Returns for %s (%s years of data)'%(symbol, round(len(pxHistory_1day)/252, 1)))
@@ -137,57 +138,67 @@ def seasonalAnalysis_overview(symbol, restrictTradingHours=False, target='close'
     # plot monthly seasonality
     #
     # on axes[0,0] barplot of seasonalAggregate_1day mean, and std dev with a secondary axis for mean
-    sns.barplot(x=seasonalAggregate_1day.index, y='pctChange_std', data=seasonalAggregate_1day, ax=axes[0], color='grey', alpha=0.5)
-    sns.barplot(x=seasonalAggregate_1day.index, y='pctChange_mean', data=seasonalAggregate_1day, ax=axes[0].twinx(), color='red')
-    axes[0].set_title('Monthly Seasonality') # set title for subplot
-    axes[0].set_xlabel('Day of Month')
+    sns.barplot(x=seasonalAggregate_1day.index, y='pctChange_std', data=seasonalAggregate_1day, ax=axes[0,0], color='grey', alpha=0.5)
+    sns.barplot(x=seasonalAggregate_1day.index, y='pctChange_mean', data=seasonalAggregate_1day, ax=axes[0,0].twinx(), color='red')
+    axes[0,0].set_title('Monthly Seasonality') # set title for subplot
+    axes[0,0].set_xlabel('Day of Month')
     # show every second x tick label
-    axes[0].set_xticks(axes[0].get_xticks()[::2])
+    axes[0,0].set_xticks(axes[0,0].get_xticks()[::2])
 
     #
     # plot intra day seasonality 
     #
     # on axes[1] barplot of seasonalAggregate_5mins mean, and std dev with a secondary axis for mean
-    sns.barplot(x=seasonalAggregate_5mins.index, y='pctChange_std', data=seasonalAggregate_5mins, ax=axes[1], color='grey', alpha=0.5)
-    sns.barplot(x=seasonalAggregate_5mins.index, y='pctChange_mean', data=seasonalAggregate_5mins, ax=axes[1].twinx(), color='red')
-    axes[1].set_title('Intra-Day Seasonality') # set title for subplot
-    # tilt x axis labels 45 degrees
-    #axes[1].set_xticklabels(axes[1].get_xticklabels(), rotation=45)
+    sns.barplot(x=seasonalAggregate_5mins.index, y='pctChange_std', data=seasonalAggregate_5mins, ax=axes[0,1], color='grey', alpha=0.5)
+    sns.barplot(x=seasonalAggregate_5mins.index, y='pctChange_mean', data=seasonalAggregate_5mins, ax=axes[0,1].twinx(), color='red')
+    axes[0,1].set_title('Intra-Day Seasonality') # set title for subplot
 
     # given that xticks are a Text Object formatted as 'HH:MM:SS', reformat the xtick labels to 'HH:MM'
-    axes[1].set_xticklabels([x.get_text()[:5] for x in axes[1].get_xticklabels()])
+    axes[0,1].set_xticklabels([x.get_text()[:5] for x in axes[0,1].get_xticklabels()])
 
     # show every second x tick label
-    axes[1].set_xticks(axes[1].get_xticks()[::8])
+    axes[0,1].set_xticks(axes[0,1].get_xticks()[::8])
 
 
     ## add vertical line at with x valus of 10:00:00, 11:30, 1:00, 2:00, 3:00
     if restrictTradingHours:
-        axes[1].axvline(x=6, color='black', linestyle='--')
-        axes[1].axvline(x=23, color='black', linestyle='--')
-        axes[1].axvline(x=40, color='black', linestyle='--')
-        axes[1].axvline(x=53, color='black', linestyle='--')
-        axes[1].axvline(x=66, color='black', linestyle='--')
+        axes[0,1].axvline(x=6, color='black', linestyle='--')
+        axes[0,1].axvline(x=23, color='black', linestyle='--')
+        axes[0,1].axvline(x=40, color='black', linestyle='--')
+        axes[0,1].axvline(x=53, color='black', linestyle='--')
+        axes[0,1].axvline(x=66, color='black', linestyle='--')
     else:
-        axes[1].axvline(x=32, color='black', linestyle='--')
-        axes[1].axvline(x=52, color='black', linestyle='--')
-        axes[1].axvline(x=82, color='black', linestyle='--')
-        axes[1].axvline(x=102, color='black', linestyle='--')
+        axes[0,1].axvline(x=32, color='black', linestyle='--')
+        axes[0,1].axvline(x=52, color='black', linestyle='--')
+        axes[0,1].axvline(x=82, color='black', linestyle='--')
+        axes[0,1].axvline(x=102, color='black', linestyle='--')
     
     #
     # plot weekly seasonality
     #
     # on axes[2] barplot of seasonalAggregate_weekByDay mean, and std dev with a secondary axis for mean
-    sns.barplot(x=seasonalAggregate_weekByDay.index, y='pctChange_std', data=seasonalAggregate_weekByDay, ax=axes[2], color='grey', alpha=0.5)
-    sns.barplot(x=seasonalAggregate_weekByDay.index, y='pctChange_mean', data=seasonalAggregate_weekByDay, ax=axes[2].twinx(), color='red')
-    axes[2].set_title('Weekly Seasonality') # set title for subplot
+    sns.barplot(x=seasonalAggregate_weekByDay.index, y='pctChange_std', data=seasonalAggregate_weekByDay, ax=axes[0,2], color='grey', alpha=0.5)
+    sns.barplot(x=seasonalAggregate_weekByDay.index, y='pctChange_mean', data=seasonalAggregate_weekByDay, ax=axes[0,2].twinx(), color='red')
+    axes[0,2].set_title('Weekly Seasonality') # set title for subplot
     # hide xaxis label
-    axes[2].set_xlabel('Day of Week')
+    axes[0,2].set_xlabel('Day of Week')
 
     # set xaxis tick labels to day of the week
-    axes[2].set_xticklabels(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'])
+    axes[0,2].set_xticklabels(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'])
 
-    plt.subplots_adjust(hspace=0.3, wspace=0.4, left=0.05, right=0.95, top=0.95, bottom=0.09)
+    #
+    # Plot year by month seasonality 
+    #
+    # on axes[1] barplot of seasonalAggregate_yearByMonth mean, and std dev with a secondary axis for mean
+    sns.barplot(x=seasonalAggregate_yearByMonth.index, y='pctChange_std', data=seasonalAggregate_yearByMonth, ax=axes[1,0], color='grey', alpha=0.5)    
+    sns.barplot(x=seasonalAggregate_yearByMonth.index, y='pctChange_mean', data=seasonalAggregate_yearByMonth, ax=axes[1,0].twinx(), color='red')
+    axes[1,0].set_title('Yearly Seasonality') # set title for subplot
+    # hide xaxis label
+    axes[1,0].set_xlabel('')
+    # set xaxis tick labels to month of the year
+    axes[1,0].set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+
+    #plt.subplots_adjust(hspace=0.3, wspace=0.4, left=0.05, right=0.95, top=0.95, bottom=0.09)
 
 
 """
@@ -211,6 +222,9 @@ def getSeasonalAggregate(pxHistory, interval, symbol, numdays=0):
     elif interval in ('weekByDay'):
         pxHistory_aggregated = pxHistory.groupby(pxHistory.index.dayofweek).agg({'pctChange':['mean', 'std']})
     
+    elif interval in ('yearByMonth'):
+        pxHistory_aggregated = pxHistory.groupby(pxHistory.index.month).agg({'pctChange':['mean', 'std']})
+
     else:
         print('aggreagation not supported for interval: '+interval)
 
@@ -307,5 +321,6 @@ restrictTradingHours = True # if true -> only analyze data between 9:30am and 4p
 seasonalAnalysis_intraday(symbol, interval, target, restrictTradingHours)
 seasonalAnalysis_overview(symbol, restrictTradingHours, target)
 
+plt.tight_layout()
 plt.show()
 
