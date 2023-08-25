6527bc452ad7e8f4d5d7be7c0a6be212ca09bbd4
doomed51 <doomed51@gmail.com>
2023-08-24 21:37:13 -0400

    added volume aggregation function

--- utils.py
+++ utils.py
@@ -20,3 +20,55 @@ def calcLogReturns(history, colName):
 """
 def futures_getExpiryDateString():
     return pd.Timestamp.today().strftime('%Y%m')
+
+"""
+    Returns volume mean and sd grouped by month
+    inputs:
+        - history: dataframe of 1day ohlcv data
+    outputs:
+        - dataframe with columns: [month, volume_mean, volume_sd]
+"""
+def aggregate_volume_month(history):
+    # throw error if interval is not 1day
+    if history['interval'][0] != '1day':
+        raise ValueError('interval must be 1day')
+    
+    # convert date column to datetime
+    history['date'] = pd.to_datetime(history['date'])
+
+    # sort by date
+    history = history.sort_values(by='date')
+
+    # add month column
+    history['month'] = history['date'].dt.month
+
+    # group by month and get mean and sd of volume
+    volume_month = history.groupby('month')['volume'].agg(['mean', 'std']).reset_index()
+
+    return volume_month
+
+"""
+    Returns volume mean and sd grouped by day of month
+    inputs:
+        - history: dataframe of 1day ohlcv data
+    outputs:
+        - dataframe with columns: [month, volume_mean, volume_sd]
+"""
+def aggregate_volume_dayOfMonth(history):
+    # throw error if interval is not 1day
+    if history['interval'][0] != '1day':
+        raise ValueError('interval must be 1day')
+        
+    # convert date column to datetime
+    history['date'] = pd.to_datetime(history['date'])
+
+    # sort by date
+    history = history.sort_values(by='date')
+
+    # add month column
+    history['dayOfMonth'] = history['date'].dt.day
+
+    # group by month and get mean and sd of volume
+    volume_dayOfMonth = history.groupby('dayOfMonth')['volume'].agg(['mean', 'std']).reset_index()
+
+    return volume_dayOfMonth
