8bf83df2dfbb1f4fe6b8b9cc4d04c31912e1b676
doomed51 <doomed51@gmail.com>
2023-12-16 08:37:51 -0500

    added index list and optimization db

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 dbname_stock = '/workbench/historicalData/venv/saveHistoricalData/historicalData_index.db'
+#dbname_stock = 'historicalData_index.db'
+
+#dbname_analysisOptimizations = '/workbench/historicalData/venv/analysisOptimizations.db'
+dbname_analysisOptimizations = 'analysisOptimizations.db'
+
+_indexList = ['VIX', 'VIX3M', 'VVIX', 'SPX', 'VIX1D', 'TSX']
