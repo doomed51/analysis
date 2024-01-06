c7dfc7b0c0544bbe0d11db48e3ab311cdebc4b07
doomed51 <doomed51@gmail.com>
2024-01-05 20:41:23 -0500

    termstructure data from local db

--- utils/utils_termStructure.py
+++ utils/utils_termStructure.py
@@ -1,26 +1,26 @@
 """
 This module consists of two functions that return VIX term structure, and contango values between contracts.
-    - getRawVixTermStructure(): returns the raw vix term structure data
-    - getVixTermStructurePctContango(): returns the percent change between n and n+1 month futures, and optionally adds columns for contango between the 4th and 7th month futures, and contango between the current and longest term future
-
     The current implementation uses vix contract data from vixcentral.com 
 """
 import pandas as pd
 
-## read in vix term structure data
-vix_ts_raw = pd.read_csv('vix.csv')
-
-# convert date column to datetime
-vix_ts_raw['date'] = pd.to_datetime(vix_ts_raw['date'])
-
-
-# set date as index
-vix_ts_raw.set_index('date', inplace=True)
-
 """ 
-Returns raw vix term structure data
+Returns raw vix term structure df
 """
-def getRawVixTermStructure():
+def getRawTermStructure(termstructure_db_conn, symbol='VIX', interval='1day'):
+    symbol = symbol.upper()
+    tablename = f'{symbol}_{interval}'
+    # read in vix term structure data
+    vix_ts_raw = pd.read_sql(f'SELECT * FROM {tablename}', termstructure_db_conn)
+
+    ## read in vix term structure data
+    #vix_ts_raw = pd.read_csv('vix.csv')
+
+    # convert date column to datetime
+    vix_ts_raw['date'] = pd.to_datetime(vix_ts_raw['date'])
+
+    # set date as index
+    vix_ts_raw.set_index('date', inplace=True)
     return vix_ts_raw
 
 """
@@ -33,18 +33,19 @@ Params:
         if True, adds a column to the dataframe that is the percent difference between the current and longest term future
 
 """
-def getVixTermStructurePctContango(fourToSeven = False, currentToLast = False, averageContango = False):
+def getVixTermStructurePctContango(vix_ts_raw, fourToSeven = False, currentToLast = False, averageContango = False):
+    
     # create a new df with the percent change between n and n+1 month futures
-    vix_ts_pctContango = vix_ts_raw.pct_change(axis='columns', periods=-1).drop(columns='8')*-1
+    vix_ts_pctContango = vix_ts_raw.pct_change(axis='columns', periods=-1).drop(columns='month8')*-1
     
     if fourToSeven:
         # add contango from the 4th to 7th month
-        vix_ts_raw['fourToSevenMoContango'] = (vix_ts_raw['6'] - vix_ts_raw['3'])/vix_ts_raw['3']
+        vix_ts_raw['fourToSevenMoContango'] = (vix_ts_raw['month7'] - vix_ts_raw['month4'])/vix_ts_raw['month4']
         vix_ts_pctContango = vix_ts_pctContango.join(vix_ts_raw['fourToSevenMoContango'], on='date')
 
     if currentToLast:
         # add contango between current and longest term future
-        vix_ts_raw['currentToLastContango'] = (vix_ts_raw['8'] - vix_ts_raw['0'])/vix_ts_raw['0']
+        vix_ts_raw['currentToLastContango'] = (vix_ts_raw['month8'] - vix_ts_raw['month1'])/vix_ts_raw['month1']
         vix_ts_pctContango = vix_ts_pctContango.join(vix_ts_raw['currentToLastContango'], on='date')
     
     if averageContango:
