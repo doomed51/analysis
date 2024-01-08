c8e8012a30e081a1baf2a16b960e0b088d187860
doomed51 <doomed51@gmail.com>
2024-01-07 23:53:25 -0500

    abstracted contango calc to any term structure data

--- utils/utils_termStructure.py
+++ utils/utils_termStructure.py
@@ -33,31 +33,31 @@ Params:
         if True, adds a column to the dataframe that is the percent difference between the current and longest term future
 
 """
-def getVixTermStructurePctContango(vix_ts_raw, oneToTwo=False, fourToSeven = False, currentToLast = False, averageContango = False):
+def getVixTermStructurePctContango(ts_raw, oneToTwo=False, fourToSeven = False, currentToLast = False, averageContango = False):
     
     # create a new df with the percent change between n and n+1 month futures
-    vix_ts_pctContango = vix_ts_raw.pct_change(axis='columns', periods=-1).drop(columns='month8')*-1
+    ts_pctContango = ts_raw.pct_change(axis='columns', periods=-1).drop(columns='month8')*-1
     
     if fourToSeven:
         # add contango from the 4th to 7th month
-        vix_ts_raw['fourToSevenMoContango'] = ((vix_ts_raw['month7'] - vix_ts_raw['month4'])/vix_ts_raw['month4'])*100
-        vix_ts_pctContango = vix_ts_pctContango.join(vix_ts_raw['fourToSevenMoContango'], on='date')
+        ts_raw['fourToSevenMoContango'] = ((ts_raw['month7'] - ts_raw['month4'])/ts_raw['month4'])*100
+        ts_pctContango = ts_pctContango.join(ts_raw['fourToSevenMoContango'], on='date')
 
     if currentToLast:
         # add contango between current and longest term future
-        vix_ts_raw['currentToLastContango'] = ((vix_ts_raw['month8'] - vix_ts_raw['month1'])/vix_ts_raw['month1'])*100
-        vix_ts_pctContango = vix_ts_pctContango.join(vix_ts_raw['currentToLastContango'], on='date')
+        ts_raw['currentToLastContango'] = ((ts_raw['month8'] - ts_raw['month1'])/ts_raw['month1'])*100
+        ts_pctContango = ts_pctContango.join(ts_raw['currentToLastContango'], on='date')
     
     if oneToTwo:
         # add contango between m1 and m2
-        vix_ts_raw['oneToTwoMoContango'] = ((vix_ts_raw['month2'] - vix_ts_raw['month1'])/vix_ts_raw['month1'])*100
-        vix_ts_pctContango = vix_ts_pctContango.join(vix_ts_raw['oneToTwoMoContango'], on='date')
+        ts_raw['oneToTwoMoContango'] = ((ts_raw['month2'] - ts_raw['month1'])/ts_raw['month1'])*100
+        ts_pctContango = ts_pctContango.join(ts_raw['oneToTwoMoContango'], on='date')
     
     if averageContango:
         # add averageContango column 
-        vix_ts_pctContango['averageContango'] = vix_ts_pctContango.mean(axis=1)
+        ts_pctContango['averageContango'] = ts_pctContango.mean(axis=1)
     
     ## sort by Date column
-    vix_ts_pctContango.sort_values(by='date', inplace=True)
-    return vix_ts_pctContango
+    ts_pctContango.sort_values(by='date', inplace=True)
+    return ts_pctContango
 
