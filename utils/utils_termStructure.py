1d5b62ab66485c01d86e6ada643e0d3a6f23baf3
doomed51 <doomed51@gmail.com>
2024-01-10 13:42:25 -0500

    removed deprecated code

--- utils/utils_termStructure.py
+++ utils/utils_termStructure.py
@@ -5,16 +5,13 @@ This module consists of two functions that return VIX term structure, and contan
 import pandas as pd
 
 """ 
-Returns raw vix term structure df
+Returns raw term structure for the supplied symbol and interval
 """
 def getRawTermStructure(termstructure_db_conn, symbol='VIX', interval='1day'):
     symbol = symbol.upper()
     tablename = f'{symbol}_{interval}'
     # read in vix term structure data
     vix_ts_raw = pd.read_sql(f'SELECT * FROM {tablename}', termstructure_db_conn)
-
-    ## read in vix term structure data
-    #vix_ts_raw = pd.read_csv('vix.csv')
 
     # convert date column to datetime
     vix_ts_raw['date'] = pd.to_datetime(vix_ts_raw['date'])
@@ -33,7 +30,7 @@ Params:
         if True, adds a column to the dataframe that is the percent difference between the current and longest term future
 
 """
-def getVixTermStructurePctContango(ts_raw, oneToTwo=False, fourToSeven = False, currentToLast = False, averageContango = False):
+def getTermStructurePctContango(ts_raw, oneToTwo=False, oneToThree=False, twoToThree = False, threeToFour= False, fourToSeven = False, currentToLast = False, averageContango = False):
     
     # create a new df with the percent change between n and n+1 month futures
     ts_pctContango = ts_raw.pct_change(axis='columns', periods=-1).drop(columns='month8')*-1
@@ -53,6 +50,21 @@ def getVixTermStructurePctContango(ts_raw, oneToTwo=False, fourToSeven = False, 
         ts_raw['oneToTwoMoContango'] = ((ts_raw['month2'] - ts_raw['month1'])/ts_raw['month1'])*100
         ts_pctContango = ts_pctContango.join(ts_raw['oneToTwoMoContango'], on='date')
     
+    if oneToThree:
+        # add contango between m1 and m3
+        ts_raw['oneToThreeMoContango'] = ((ts_raw['month3'] - ts_raw['month1'])/ts_raw['month1'])*100
+        ts_pctContango = ts_pctContango.join(ts_raw['oneToThreeMoContango'], on='date')
+    
+    if twoToThree: 
+        # add contango between m2 and m3
+        ts_raw['twoToThreeMoContango'] = ((ts_raw['month3'] - ts_raw['month2'])/ts_raw['month2'])*100
+        ts_pctContango = ts_pctContango.join(ts_raw['twoToThreeMoContango'], on='date')
+    
+    if threeToFour:
+        # add contango between m3 and m4
+        ts_raw['threeToFourMoContango'] = ((ts_raw['month4'] - ts_raw['month3'])/ts_raw['month3'])*100
+        ts_pctContango = ts_pctContango.join(ts_raw['threeToFourMoContango'], on='date')
+
     if averageContango:
         # add averageContango column 
         ts_pctContango['averageContango'] = ts_pctContango.mean(axis=1)
