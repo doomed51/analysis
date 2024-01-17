ef15238ece318276b40d2a5c5aadcc0ee6d7756b
doomed51 <doomed51@gmail.com>
2024-01-16 20:59:01 -0500

    removed hardcoded signal column name when plotting returns heatmap

--- returns/strategy_crossover.py
+++ returns/strategy_crossover.py
@@ -13,8 +13,8 @@ class CrossoverStrategy:
     def __init__(self, base_df, signal_df, target_column_name, signal_column_name):
         self.target_column_name = target_column_name
         self.signal_column_name = signal_column_name
-        self.base_df = base_df
-        self.signal_df = self._calculateSignal(signal_df)
+        self.base_df = base_df.reset_index(drop=True)
+        self.signal_df = self._calculateSignal(signal_df).reset_index(drop=True)
 
     def _calculateSignal(self, signal_df):
         # calculated as target column - signal column
@@ -47,45 +47,59 @@ class CrossoverStrategy:
 
     def plotSignalOverview(self): 
         fig, ax = plt.subplots(2,2)
-        fig.suptitle('Signal Overview')
+        fig.suptitle('%s Signal Overview'%(self.base_df['symbol'][0]))
         
         # plot signal returns heatmap
-        self.drawSignalReturnsHeatmap(ax[0,0], maxperiod_fwdreturns=100)
+        self.drawSignalReturnsHeatmap(ax[0,0], maxperiod_fwdreturns=100, signal_columnName=self.signal_column_name)
         
+        # plot the underlying, and the components of the signal
         self.drawBaseAndSignal(ax[0,1])
-        ax[0,1].legend(loc='upper left')
+        #ax[0,1].legend(loc='upper left')
         
+        # plot autocorrelation of signal 
+        ##autocorrelations = sa.calculateAutocorrelations(self.signal_df, self.signal_column_name)
+        ##ax[1,0].stem(autocorrelations, linefmt='--')
+        ##ax[1,0].set_title('Signal Autocorrelation')
 
-        autocorrelations = sa.calculateAutocorrelations(self.signal_df, self.signal_column_name)
-        ax[1,0].stem(autocorrelations, linefmt='--')
-
+        # plot the signal against its percentile bounds
         self.drawSignalAndBounds(ax[1,1])
 
+        #signal_mod1 = self.signal_df.copy() 
+        # change the signal column to be 1 if columnval >0, -1 if the columnval <0
+        print('%s_normalized'%(self.signal_column_name))
+        #self.signal_df['%s_normalized'%(self.signal_column_name)] = self.signal_df['%s_normalized'%(self.signal_column_name)].apply(lambda x: 1 if x > 0 else -1)
+        self.signal_df['sma_normalized']=self.signal_df['sma_normalized'].apply(lambda x: 1 if x > 0 else -1)
+        self.drawSignalReturnsHeatmap(ax[1,0], maxperiod_fwdreturns=100, signal_columnName='sma_normalized')
+        
         fig.tight_layout()
 
         return fig
     
-    def plotSignalReturnsHeatmap(self, maxperiod_fwdreturns=100):
+    def plotSignalReturnsHeatmap(self, signal_columnName, maxperiod_fwdreturns=100):
         fig, ax = plt.subplots()
         fig.suptitle('Signal Returns Heatmap')
-        self.drawSignalReturnsHeatmap(ax, maxperiod_fwdreturns)
+        self.drawSignalReturnsHeatmap(ax, maxperiod_fwdreturns, signal_columnName)
         return fig
 
-    def drawSignalReturnsHeatmap(self, ax, maxperiod_fwdreturns):        
+    def drawSignalReturnsHeatmap(self, ax, signal_columnName, maxperiod_fwdreturns):        
         signal_rounding = 2 # how much to bucket together the signal column
-        mean_fwdReturns = sa.bucketAndCalcSignalReturns(self.signal_df, self.signal_column_name, signal_rounding, maxperiod_fwdreturns)
+        mean_fwdReturns = sa.bucketAndCalcSignalReturns(self.signal_df, signal_columnName, signal_rounding, maxperiod_fwdreturns)
         sns.heatmap(mean_fwdReturns, annot=False, cmap='RdYlGn', ax=ax)
+
+        # set title
+        ax.set_title('Signal vs. Fwd. Returns')
 
     """
         Plots the base and signal timeseries on the provided axis
     """
     def drawBaseAndSignal(self, ax): 
         # set title
-        ax.set_title('Base vs. Signal')
+        ax.set_title('Underlying vs. Signal')
 
         # plot the base and signal timeseries 
         sns.lineplot(x=self.base_df['date'], y=self.base_df[self.target_column_name], ax=ax, label=self.target_column_name)
         sns.lineplot(x=self.signal_df['date'], y=self.signal_df[self.signal_column_name], ax=ax, label=self.signal_column_name)
+        ax.legend(loc='upper left')
 
         # plot the underlying 
         ax2 = ax.twinx()
@@ -108,18 +122,22 @@ class CrossoverStrategy:
 
         # plot signal on bottom plot
         #ax.plot(self.signal_df['date'], self.signal_df['signal'], label='signal')
-        sns.lineplot(x=self.signal_df['date'], y=self.signal_df['signal'], ax=ax, label='signal')
+        sns.lineplot(x=self.signal_df['date'], y=self.signal_df[self.signal_column_name], ax=ax, label='signal')
 
         # add rolling percentile lines
-        sns.lineplot(x=self.signal_df['date'], y=self.signal_df['signal'].rolling(252).quantile(upperbound), ax=ax, label='90th percentile', color='red', alpha=0.3)
-        sns.lineplot(x=self.signal_df['date'], y=self.signal_df['signal'].rolling(252).quantile(lowerbound), ax=ax, label='10th percentile', color='red', alpha=0.3)
+        sns.lineplot(x=self.signal_df['date'], y=self.signal_df[self.signal_column_name].rolling(252).quantile(upperbound), ax=ax, label='90th percentile', color='red', alpha=0.3)
+        sns.lineplot(x=self.signal_df['date'], y=self.signal_df[self.signal_column_name].rolling(252).quantile(lowerbound), ax=ax, label='10th percentile', color='red', alpha=0.3)
 
         # add percentile labels
-        ax.text(self.signal_df['date'].iloc[0], self.signal_df['signal'].quantile(upperbound), '%s percentile: %0.5f'%(int(upperbound*100), self.signal_df['signal'].quantile(upperbound)), color='red', fontsize=10)
-        ax.text(self.signal_df['date'].iloc[0], self.signal_df['signal'].quantile(lowerbound), '%s percentile: %0.5f'%(int(lowerbound*100), self.signal_df['signal'].quantile(upperbound)), color='red', fontsize=10)
+        ax.text(self.signal_df['date'].iloc[0], self.signal_df[self.signal_column_name].quantile(upperbound), '%s percentile: %0.5f'%(int(upperbound*100), self.signal_df['signal'].quantile(upperbound)), color='red', fontsize=10)
+        ax.text(self.signal_df['date'].iloc[0], self.signal_df[self.signal_column_name].quantile(lowerbound), '%s percentile: %0.5f'%(int(lowerbound*100), self.signal_df['signal'].quantile(upperbound)), color='red', fontsize=10)
 
         # format plot
         ax.grid(True, which='both', axis='both', linestyle='-', alpha=0.2)
         ax.axhline(0, color='grey', linestyle='-', alpha=0.5)
         ax.legend(loc='upper left')
 
+        # plot undelying close on a secondary axis
+        ax2 = ax.twinx()
+        sns.lineplot(x=self.base_df['date'], y=self.base_df['close'], ax=ax2, color='black', label='%s close'%(self.base_df['symbol'][0]))
+        ax2.legend(loc='upper right')
