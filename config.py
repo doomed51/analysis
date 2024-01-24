6a791a35570f0508fb455cf632d5cf9f1b319e37
doomed51 <doomed51@gmail.com>
2024-01-23 20:21:55 -0500

    fixed formatting

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
+#############
+############# DB Paths
+
 dbname_stock = '/workbench/historicalData/venv/saveHistoricalData/data/historicalData_index.db'
 dbname_termstructure = '/workbench/historicalData/venv/saveHistoricalData/data/termstructure.db'
-
 #dbname_analysisOptimizations = '/workbench/historicalData/venv/analysisOptimizations.db'
 dbname_analysisOptimizations = 'analysisOptimizations.db'
 
+#______________
+###############
+
+###############
+############### Reference Lists
+
 _indexList = ['VIX', 'VIX3M', 'VVIX', 'SPX', 'VIX1D', 'TSX']
+
+#______________
+###############
