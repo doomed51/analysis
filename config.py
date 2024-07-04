b10ba985575c630eee4576ed1bb3e93a41d7a8a2
doomed51 <doomed51@gmail.com>
2024-07-04 09:08:39 -0400

    added exchange and currency mappings

--- config.py
+++ config.py
@@ -8,3 +8,65 @@ dbname_analysisOptimizations = 'analysisOptimizations.db'
 ############### Reference Lists
 
 _indexList = ['VIX', 'VIX3M', 'VVIX', 'SPX', 'VIX1D', 'TSX']
+
+
+currency_mapping = {
+    'CL': 'USD',
+    'DXJ': 'USD',
+    'ED': 'USD',
+    'ES': 'USD',
+    'ENB': 'CAD',
+    'FV': 'USD',
+    'GC': 'USD',
+    'GE': 'USD',
+    'HG': 'USD',
+    'NQ': 'USD',
+    'SPX': 'USD',
+    'SI': 'USD',
+    'TN': 'USD',
+    'TSX': 'CAD',
+    'TY': 'USD',
+    'UB': 'USD',
+    'US': 'USD',
+    'VIX': 'USD',
+    'VIX1D': 'USD',
+    'VIX3M': 'USD',
+    'VVIX': 'USD',
+    'XIU': 'CAD',
+    'Z': 'USD',
+    'ZB': 'USD',
+    'ZF': 'USD',
+    'ZN': 'USD',
+    'ZT': 'USD'
+}
+
+exchange_mapping = {
+    'CL': 'NYMEX',
+    'DXJ': 'ARCA',
+    'ED': 'GLOBEX',
+    'ES': 'GLOBEX',
+    'ENB': 'SMART',
+    'FV': 'ECBOT',
+    'GC': 'NYMEX',
+    'GE': 'ECBOT',
+    'HG': 'COMEX',
+    'IBM': 'NYSE',
+    'NQ': 'GLOBEX',
+    'SPX': 'CBOE',
+    'SI': 'NYMEX',
+    'TN': 'ECBOT',
+    'TSX': 'TSE',
+    'TY': 'ECBOT',
+    'UB': 'ECBOT',
+    'US': 'ECBOT',
+    'VIX': 'CBOE',
+    'VIX1D': 'CBOE',
+    'VIX3M': 'CBOE',
+    'VVIX': 'CBOE',
+    'XIU': 'SMART',
+    'Z': 'ECBOT',
+    'ZB': 'ECBOT',
+    'ZF': 'ECBOT',
+    'ZN': 'ECBOT',
+    'ZT': 'ECBOT'
+}
