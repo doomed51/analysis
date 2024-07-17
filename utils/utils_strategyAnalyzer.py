8d49bd4b2816f54d82f914b97b5737158b27567e
doomed51 <doomed51@gmail.com>
2024-07-17 13:06:07 -0400

    updated docs

--- utils/utils_strategyAnalyzer.py
+++ utils/utils_strategyAnalyzer.py
@@ -1,10 +1,5 @@
-# Dataframe A with a column to be used as the "signal"
-#    assume the signal df has close px to calculate fwd returrns 
-# add n number of forward returns columns
-# round signal column to n decimal places, or otherwise bucket it to calculate the mean fwd returns on
-# calculate means for each fwdreturn column grouped by signal column
-# 
 import numpy as np
+
 """
     Calculates autocrrelations for a given dataframe and target column
 """
@@ -15,15 +10,15 @@ def calculateAutocorrelations(df, targetColName, max_lag=100):
         autocorrelations.append(df[targetColName].autocorr(lag=i))
     return autocorrelations
 
-"""
-    Buckets by signal and calculates the mean fwdReturns for each period 
-    inputs:
-        signaldf: dataframe with a column to be used as the "signal", and close px to calculate fwd returrns
-        signal_col: identifies the column to be used as the signal 
-        signal_rounding: (optional) Round the signal column to n decimal places, default is 2
-        maxperiod_fwdreturns: (optional) number of fwd returns columns to add to the dataframe, default 2
-"""
-def bucketAndCalcSignalReturns(signaldf, signal_col, signal_rounding=2, maxperiod_fwdreturns=50):
+def bucketAndCalcSignalReturns(signaldf, signal_col, signal_rounding=1, maxperiod_fwdreturns=20):
+    """
+        Buckets by signal and calculates the mean fwdReturns for each period 
+        inputs:
+            signaldf: dataframe with a column to be used as the "signal", and close px to calculate fwd returrns
+            signal_col: identifies the column to be used as the signal 
+            signal_rounding: (optional) Round the signal column to n decimal places, default is 2
+            maxperiod_fwdreturns: (optional) number of fwd returns columns to add to the dataframe, default 2
+    """
     
     signaldf.dropna(subset=[signal_col], inplace=True)
     
